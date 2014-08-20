59b4059ac44125dacf12cb6d30fc06b02154eb4c
alex <alexr.3165@gmail.com>
2014-08-20 19:31:00 +0200

    add files to gitignore

--- sondage/web/js/app.js
+++ sondage/web/js/app.js
@@ -2,6 +2,25 @@ var ccApp = angular.module('numeractive', ['ui.router']);
 
 ccApp.config(['$urlRouterProvider', '$stateProvider', '$provide',
 	function($urlRouterProvider, $stateProvider, $provide) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+		.state('timeline',{
+			url: '/timeline',
+			templateUrl: '/timeline',
+			controller: 'timeline'
+		})
+		.state('slide',{
+			url: '/slide/:id',
+			templateUrl: '/slide/id',
+			controller: 'slide'
+		})
 
 	}
 ]);
+
+ccApp.controller('timeline', ['$scope', function($scope){
+
+
+
+}]);
